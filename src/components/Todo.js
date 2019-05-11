db0fd620ae337fa08681f5265d792a2763795874
Tarcísio Pedro <tarcisiopgs@gmail.com>
2019-05-11 11:41:13 -0300

    repo update

---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import PropTypes from 'prop-types';
+
+const Todo = ({title}) => (
+    <View>
+        <Text>{title}</Text>
+    </View>
+);
+
+Todo.defaultProps = {
+    title: 'Todo padrão'
+};
+
+Todo.propTypes = {
+    title: PropTypes.string,
+};
+
+export default Todo;
---
+++ src/index.js
@@ -0,0 +1,66 @@
+import '~/config/ReactotronConfig';
+import '~/config/DevToolsConfig';
+
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import Todo from "~/components/Todo";
+
+export default class App extends Component {
+  state = {
+    usuario: 'Tarcísio',
+    todos: [
+      {
+        id: 0,
+        text: 'Fazer café',
+      },
+      {
+        id: 1,
+        text: 'Estudar o GoNative'
+      }
+    ]
+  };
+
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, {id: Math.random(), text: 'Novo todo'}],
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Todo title="Fazer café" />
+        <Todo title="Fazer café" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  box: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#F00',
+    margin: 10,
+    transform: [
+      {
+        rotateZ: '20deg'
+      }
+    ]
+  },
+  boxText: {
+    color: '#fff',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  }
+});
